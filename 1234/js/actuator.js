06ccc4ab274a44baa20bdd32c98d3230c77f4e59
Andrew Alcutt <aralcutt@gmail.com>
2016-11-30 22:48:29 -0500

    updating 1234. removing 1234b

--- 1234/js/actuator.js
+++ 1234/js/actuator.js
@@ -22,9 +22,9 @@ HTMLActuator.prototype.actuate = function(grid, meta){
       for(var y = 0; y < grid.size; y++){
         var cell = grid.cells[x][y];
         if(cell){
-          s += "<td class='tile' x='{x}' y='{y}'>{0}{1}</td>"
+          s += "<td class='tile value-{0}' x='{x}' y='{y}'>{1}</td>"
             .replace("{0}", cell.value)
-            .replace("{1}", self.getArrow(cell.direction))
+            .replace("{1}", self.getArrow(cell.direction, cell.value))
             .replace("{x}", cell.x).replace("{y}", cell.y)
         }
         else{
@@ -57,57 +57,41 @@ HTMLActuator.prototype.clearContainer = function (container) {
   container.html("");
 };
 
-HTMLActuator.prototype.getArrow = function(direction){
-  if(direction == "up"){
-    return "<span class='up-arrow'>&uarr;</span>";
+HTMLActuator.prototype.getArrow = function(direction, value){
+  var arrowString = "";
+  for(var arrows = 0; arrows < value; arrows++){
+    if(direction == "up"){
+      arrowString += arrows > 0 ? "<br />" : "";
+      arrowString += "<i class='fa fa-chevron-up' aria-hidden='true'></i>";
+    }
+    if(direction == "down"){
+      arrowString += arrows > 0 ? "<br />" : "";
+      arrowString +=  "<i class='fa fa-chevron-down' aria-hidden='true'></i>";
+    }
+    if(direction == "left"){
+      arrowString +=  "<i class='fa fa-chevron-left' aria-hidden='true'></i>";
+    }
+    if(direction == "right"){
+      arrowString +=  "<i class='fa fa-chevron-right' aria-hidden='true'></i>";
+    }
   }
-  if(direction == "down"){
-    return "<span class='down-arrow'>&darr;</span>";
-  }
-  if(direction == "left"){
-    return "<span class='left-arrow'>&larr;</span>";
-  }
-  if(direction == "right"){
-    return "<span class='right-arrow'>&rarr;</span>";
-  }
-}
-
-HTMLActuator.prototype.addTile = function (tile) {
-  var self = this;
-
-  var wrapper   = document.createElement("div");
-  var inner     = document.createElement("div");
-  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
-  inner.textContent = tile.value;
-  wrapper.appendChild(inner);
-  // Put the tile on the board
-  this.tileContainer.append(wrapper);
+  return arrowString;
 }
 
 HTMLActuator.prototype.updateScore = function(score, puzzleBest){
   this.clearContainer(this.scoreContainer);
   this.score = score;
-  this.scoreContainer.text(score);
+  this.scoreContainer.text("Total: " + score);
   this.puzzleBest = puzzleBest != null ? puzzleBest : "NA";
-  $('.best-score-container').text(this.puzzleBest);
+  $('.best-score-container').text("Lowest: " + this.puzzleBest);
 }
 
 HTMLActuator.prototype.message = function(won){
   var type = won ? "game-won" : "game-over";
-  var message =  "Better luck next time";
-  if(this.score <= 52){
-    message = "Amazing!";
-  }
-  else if (this.score >= 53 && this.score <= 60){
-    message = "Great!";
-  }
-  else if(this.score >= 61 && this.score <= 65){
-    message = "Average";
-  }
 
   this.messageContainer.addClass(type);
-  this.messageContainer.find('p').text(message);
-  this.messageContainer.find('.score').html("Your score: <strong> " + this.score + "</strong>");
+  this.messageContainer.find('p').text("Fin");
+  this.messageContainer.find('.score').html("Your total: <strong> " + this.score + "</strong>");
 
   var shareLink = location.protocol+'//'+location.host+location.pathname + "#" + this.puzzleCode;
   this.shareContainer.find('.puzzle-link').val(shareLink)
